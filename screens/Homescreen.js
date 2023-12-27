428d16142e507562823ac0a8805003479d4785da
Harry-Kwesi <harrydegraft@hotmail.com>
2023-12-27 19:46:25 +0530

    fixed long height issue with the header

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -19,37 +19,35 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView className="bg-white ">
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-          <Image
-            source={{
-              uri: "https://shorturl.at/dnwIO",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      <View className="flex-row pb-2 items-center mx-4 space-x-2 px-2">
+        <Image
+          source={{
+            uri: "https://shorturl.at/dnwIO",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      <View className="flex-row  items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
           />
-
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color="#00CCBB" />
         </View>
-
-        <View className="flex-row  items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3">
-            <MagnifyingGlassIcon size={20} color="gray" />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsHorizontalIcon color="#00CCBB" />
-        </View>
-      </Text>
+        <AdjustmentsHorizontalIcon color="#00CCBB" />
+      </View>
     </SafeAreaView>
   );
 }
